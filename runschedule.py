65abcb9ea0750aec9c6ce9ba55338f8bc8761446
Samuel Dockery <samueldockery@gmail.com>
2023-11-05 10:40:36 -0800

    tweak description

--- runschedule.py
+++ runschedule.py
@@ -5,7 +5,7 @@ import argparse
 from rsclient import runSchedule
 
 def main():
-  argParser = argparse.ArgumentParser(description="Raspberry Sprinkles Run Adhoc Existing Schedule(s)")
+  argParser = argparse.ArgumentParser(description="Raspberry Sprinkles Run Existing Schedule(s)")
   argParser.add_argument("-s", "--schedules", help="schedules to run", type=int, nargs='+', required=True)
   argParser.add_argument("-u", "--url", help="base url", default='http://127.0.0.1:5000/')
   parsedArgs = argParser.parse_args()
